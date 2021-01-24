b0e54c8e8a0099d4788811ecdd073a7af150e65f
Juan Garcia <juagarca@gmail.com>
2021-01-24 12:50:02 +0000

    index created

--- app.rb
+++ app.rb
@@ -2,11 +2,25 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require_relative "./lib/cookbook"
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Hello world!'
+  cookbook = Cookbook.new('./lib/recipes.csv')
+  @recipes = cookbook.all
+  # binding.pry
+  erb :index
+end
+
+get '/new' do
+end
+
+get '/team/:username' do
+  binding.pry
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
