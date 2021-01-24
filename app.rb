ebd923651b43fb3a67f4c04e794a165904cdbfce
Juan Garcia <juagarca@gmail.com>
2021-01-24 13:08:54 +0000

    create action coded

--- app.rb
+++ app.rb
@@ -3,20 +3,31 @@ require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
 require_relative "./lib/cookbook"
+require_relative "./lib/recipe"
 
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+def initialize
+  @cookbook = Cookbook.new('./lib/recipes.csv')
+end
+
 get '/' do
-  cookbook = Cookbook.new('./lib/recipes.csv')
-  @recipes = cookbook.all
+  @recipes = @cookbook.all
   # binding.pry
   erb :index
 end
 
 get '/new' do
+  erb :create
+end
+
+get '/recipes' do
+  recipe = Recipe.new(name: params[:name], description: params[:description])
+  @cookbook.add(recipe)
+  redirect '/'
 end
 
 get '/team/:username' do
